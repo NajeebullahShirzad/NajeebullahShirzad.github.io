9b227734fb86b05835a13d6cf10ff71a1f812416
Najeebullah Shirzad <shrnbl95t28z200n@studenti.unical.it>
2025-12-07 19:40:24 -0500

    Updated navbar

--- js/main.js
+++ js/main.js
@@ -23,10 +23,4 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
 });
-  document.addEventListener('DOMContentLoaded', function () {
-      const toggle = document.querySelector('.nav-toggle');
-      const navLinks = document.querySelector('.nav-links');
-      if (toggle && navLinks) {
-        toggle.addEventListener('click', () => navLinks.classList.toggle('active'));
-      }
-    });
+
